0dbd26403028d7ba9587b0fedea02774931a9a3e
Robert (Meep) Zhou <meep.zh@gmail.com>
2017-03-07 00:05:39 -0500

    Session save updates user totalTime

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,8 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.afterSave('Session', function(request) {
+  request.user.increment('totalTime', request.object.get('finishTime'));
+  request.user.save();
+});
